0a05eef450c3285401b248bd585da3d15d83903e
leelakrishna002 <leelakrishna@gmail.com>
2025-08-01 14:04:45 +0530

    Fix: Updated Download CV link to direct Google Drive download

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -15,7 +15,7 @@ export const Home = () => {
 
                 <div className="flex justify-center space-x-4">
                     <a
-                    href="https://drive.google.com/uc?export=download&id=1Af0qNoG2uQOFH15_iXl1Tv_WT54qUXUK"
+                    href="https://drive.google.com/file/d/1Af0qNoG2uQOFH15_iXl1Tv_WT54qUXUK/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
