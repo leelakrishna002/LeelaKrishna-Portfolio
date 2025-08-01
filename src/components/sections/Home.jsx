5aa20933c541ebeb6b7e1a478c727d11383bc7df
leelakrishna002 <leelakrishna@gmail.com>
2025-08-01 13:53:11 +0530

    Update: Changed CV download link to Google Drive

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -14,9 +14,15 @@ export const Home = () => {
                 </p>
 
                 <div className="flex justify-center space-x-4">
-                    <a href="https://drive.google.com/file/d/1Af0qNoG2uQOFH15_iXl1Tv_WT54qUXUK/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
-                    Download CV
+                    <a
+                    href="https://drive.google.com/uc?export=download&id=1Af0qNoG2uQOFH15_iXl1Tv_WT54qUXUK"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+                    >
+                        Download CV
                     </a>
+
                             
                     <a href="#projects" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
                     View Projects
